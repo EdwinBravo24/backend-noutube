aa7cdd0c226e4aecff6ad39ff47e60c77cc6289f
Edwin Bravo <edwinbravo2121@gmail.com>
2024-12-03 01:59:49 -0500

    arreglos de cors

--- index.js
+++ index.js
@@ -15,8 +15,14 @@ const PORT = process.env.PORT || 4000;
 app.use(express.json());
 app.use(urlencoded({ extended: true }));
 app.use(cors({
-    origin: ['https://frontend-noutube.vercel.app/', 'http://localhost:4000'], // Cambia esto con tus dominios
-  }));
+    origin: [
+        'https://frontend-noutube.vercel.app', // Asegúrate de que este sea el dominio correcto de tu frontend en Vercel
+        'http://localhost:4000', // Para desarrollo local, si es necesario
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Asegúrate de que los métodos necesarios estén permitidos
+    allowedHeaders: ['Content-Type', 'Authorization'], // Asegúrate de que los headers que usas estén permitidos
+    credentials: true // Si estás utilizando cookies o autenticación basada en sesiones
+}));
 // Conexión a MongoDB
 mongoose.connect(process.env.MONGO_URI, {})
     .then(() => console.log('Conectado a MongoDB Atlas'))
